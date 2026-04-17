426d98aa87be5ddaffc5c40842bb46992d8e1de3
Aryan Sharma <aryansharma9100ji@gmail.com>
2026-04-17 10:16:19 +0530

    clean initial commit

---
+++ backend/src/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cookieParser = require("cookie-parser") // It allows your server to read cookies from the request
+const authRoutes = require("./routes/auth.routes.js")
+
+const app = express();
+app.use(express.json()); // It allows your server to read JSON data from the request body
+app.use(cookieParser()); // It allows your server to read cookies from the request
+
+app.use("/api/auth", authRoutes);
+
+module.exports = app;
---
+++ backend/src/controller/auth.controller.js
@@ -0,0 +1,5 @@
+const userModel = require("../models/user.model.js")
+
+async function register (req, res){
+    
+}
